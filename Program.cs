5eb1a78a0dacac4b9bd416c3ba6cc6f093f8730d
marshallg02 <122751535+marshallg02@users.noreply.github.com>
2023-02-08 18:34:12 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fibseqdeliverable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 25;
+            int[] fibinachinum = new int[size];
+            fibinachinum[0] = 0;
+            fibinachinum[1] = 1;
+            for (int i = 2; i < size; i++)
+            {
+                fibinachinum[i] = fibinachinum[i - 2] + fibinachinum[i - 1];
+            }
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine("Fib number  (" + i + ") = " + fibinachinum[i]);
+            }
+        }
+    }
+}
